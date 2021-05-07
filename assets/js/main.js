4d746c8c3f9abe72345cd412ece7e61876310628
gicunho <gicunho@hotmail.it>
2021-05-07 18:23:49 +0200

    Aggiunto stampa direttamente su HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,10 @@ var studente = {
 //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 for (var key in studente){
     console.log(key, studente[key]);
+    document.getElementById('studente_capo').insertAdjacentHTML('beforeend', 
+    `
+    <li> ${key}: ${studente[key]} </li>
+    `)
 }
 //Creare un array di oggetti di studenti.
 var studenti = [
@@ -28,20 +32,11 @@ var studenti = [
     }
 ]
 
-//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-for (var i = 0; i < studenti.length; i++){
-    var studenteSingolo = studenti[i];
-    console.log(studenteSingolo.nome, studenteSingolo.cognome);
-/*     for (var key in studenteSingolo) {
-        console.log(key, studenteSingolo[key]);
-    } */
-}
+
 //Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 var nomeNuovo = prompt('Inserisci il nome del nuovo studente'),
     cognomeNuovo = prompt('Inserisci il cognome del nuovo studente'),
     etàNuova = Number(prompt('Inserisci l\'età del nuovo studente'));
-
-
 
 var nuovoStudente = {
     nome: nomeNuovo,
@@ -50,3 +45,18 @@ var nuovoStudente = {
 };
 studenti.push(nuovoStudente);
 console.log(studenti);
+
+//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for (var i = 0; i < studenti.length; i++){
+    var studenteSingolo = studenti[i];
+    //console.log(studenteSingolo.nome, studenteSingolo.cognome);
+    for (var key in studenteSingolo) {
+        if (key != 'età'){
+            console.log(key, studenteSingolo[key]);
+            document.getElementById('studenti_classe').insertAdjacentHTML('beforeend', 
+            `
+            <li> ${key}: ${studenteSingolo[key]} </li>
+            `)
+        }
+    }
+}
